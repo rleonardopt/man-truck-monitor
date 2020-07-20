964a813460551b515753f15ad4ef625fc3328a22
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-07-20 17:07:32 +0100

    Init Spring Boot project

---
+++ truck-monitor/src/main/java/pt/vwds/truckmonitor/TruckMonitorApplication.java
@@ -0,0 +1,13 @@
+package pt.vwds.truckmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckMonitorApplication.class, args);
+	}
+
+}
---
+++ truck-monitor/src/test/java/pt/vwds/truckmonitor/TruckMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.vwds.truckmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
