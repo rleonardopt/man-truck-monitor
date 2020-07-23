c2d7cce9513c76e7b8ebe8103c626beb6ca668f9
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-07-23 14:30:38 +0100

    Configures the base packages used by auto-configuration when scanning for entity classes and repositories

--- truck-monitor/src/main/java/pt/vwds/truckmonitor/TruckMonitorApplication.java
+++ truck-monitor/src/main/java/pt/vwds/truckmonitor/TruckMonitorApplication.java
@@ -2,12 +2,14 @@ package pt.vwds.truckmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("pt.vwds.truckmonitor.model")
+@EnableJpaRepositories("pt.vwds.truckmonitor.repository")
 public class TruckMonitorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TruckMonitorApplication.class, args);
 	}
-
 }
