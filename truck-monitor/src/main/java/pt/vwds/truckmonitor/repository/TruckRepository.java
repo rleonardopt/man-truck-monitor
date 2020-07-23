5e1b8004e2841d92c780a467a18bc1de9e10e6ee
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-07-23 14:38:57 +0100

    Repository created for Truck

---
+++ truck-monitor/src/main/java/pt/vwds/truckmonitor/repository/TruckRepository.java
@@ -0,0 +1,13 @@
+package pt.vwds.truckmonitor.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pt.vwds.truckmonitor.model.Truck;
+
+import java.util.Optional;
+
+@Repository
+public interface TruckRepository extends CrudRepository<Truck, Long> {
+
+    Optional<Truck> findByLicensePlate(String license);
+}
